c472315b29df18d9df239fa1623791205806de2d
Shengli Jiang <44488777+sjiang87@users.noreply.github.com>
2025-03-14 09:51:49 -0400

    gnn model update

--- gcgnn/models/gnn_models.py
+++ gcgnn/models/gnn_models.py
@@ -8,8 +8,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from torch_geometric.nn import GINConv
+from torch_geometric.nn import GINConv, GCNConv, GATConv
 from torch_geometric.nn.aggr import AttentionalAggregation
+from torch_geometric.nn.pool import global_mean_pool
 
 
 def GIN_kernel(dim):
@@ -33,18 +34,26 @@ def attn_kernel(dim):
     
 class GNN(nn.Module):
     """ Pure data-driven GNN model """
-    def __init__(self, input_dim, dim, output_dim):
+    def __init__(self, input_dim, dim, output_dim, readout='attn', kernel='GIN'):
         super().__init__()
-        self.nn1 = GIN_kernel(dim)
-        self.conv1 = GINConv(self.nn1)
+        if kernel == 'GIN':
+            self.nn1 = GIN_kernel(dim)
+            self.conv1 = GINConv(self.nn1)
+        elif kernel == 'GCN':
+            self.conv1 = GCNConv(input_dim, dim)
+        elif kernel == 'GAT':
+            self.conv1 = GATConv(input_dim, dim)
         self.fc1 = nn.Linear(dim, output_dim)
-        self.attention = attn_kernel(dim)
-        
+        if readout == "attn":
+            self.readout = attn_kernel(dim)
+        elif readout == "mean":
+            self.readout = global_mean_pool
+            
 
     def forward(self, data):
         x, edge_index, batch, base = data.x, data.edge_index, data.batch, data.base
         x = F.relu(self.conv1(x, edge_index))
-        x = self.attention(x, batch)
+        x = self.readout(x, batch)
         x = self.fc1(x)
         
         return x, None, None
@@ -52,7 +61,7 @@ class GNN(nn.Module):
     
 class Baseline(nn.Module):
     """ Baseline GC model """
-    def __init__(self, input_dim, dim, output_dim):
+    def __init__(self, input_dim, dim, output_dim, readout='mean', kernel='GIN'):
         super().__init__()
         self.input_dim = input_dim
         self.dim = dim
@@ -66,19 +75,30 @@ class Baseline(nn.Module):
     
 class GNN_Guided_Baseline_Simple(nn.Module):
     """ GNN guided baseline model (GC-GNN)"""
-    def __init__(self, input_dim, dim, output_dim):
+    def __init__(self, input_dim, dim, output_dim, readout='attn', kernel='GIN'):
         super().__init__()
-        self.nn1 = GIN_kernel(dim)
-        self.conv1 = GINConv(self.nn1)
+        if kernel == 'GIN':
+            self.nn1 = GIN_kernel(dim)
+            self.conv1 = GINConv(self.nn1)
+        elif kernel == 'GCN':
+            self.conv1 = GCNConv(input_dim, dim)
+        elif kernel == 'GAT':
+            self.conv1 = GATConv(input_dim, dim)
+            
         self.fc1 = nn.Linear(dim, output_dim)
         self.fc2 = nn.Linear(dim, output_dim)
-        self.attention = attn_kernel(dim)
+        
+        if readout == "attn":
+            self.readout = attn_kernel(dim)
+        elif readout == "mean":
+            self.readout = global_mean_pool
 
     def forward(self, data):
         x, edge_index, batch, base = data.x, data.edge_index, data.batch, data.base
         x = F.relu(self.conv1(x, edge_index))
-        x = self.attention(x, batch)
+        x = self.readout(x, batch)
         a = self.fc1(x)
         b = self.fc2(x)
         x = a * base + b
         return x, a, b
+    
